a2f733d5d296569938ff3fc15ee587d4a5e2010d
Tobias Munk <schmunk@usrbin.de>
2017-02-27 20:07:39 +0100

    updated ENV variable

--- Repository/Vcs/GitDriver.php
+++ Repository/Vcs/GitDriver.php
@@ -41,7 +41,7 @@ class GitDriver extends BaseGitDriver
      */
     public function initialize()
     {
-        if (getenv('FXP_SKIP_ASSET_UPDATE')) {
+        if (getenv('FXP_ASSET__GIT_SKIP_UPDATE') != 0) {
             $localUrl = $this->config->get('cache-vcs-dir') . '/' . preg_replace('{[^a-z0-9.]}i', '-', $this->url) . '/';
             if (is_dir($localUrl) && filemtime($localUrl) > strtotime('-'.getenv('FXP_SKIP_ASSET_UPDATE'))) {
                 $this->io->write('Skipped update for '.$localUrl.', using local clone...');
